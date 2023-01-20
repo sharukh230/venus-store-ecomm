a6cdbd4073d63dca7f97fa4adc282f31ffba9952
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2023-01-20 17:38:32 +0530

    file updated

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Route,Routes,
+  // redirect,
+} from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        {/* <Route exact path="/">
+          <Home />
+        </Route> */}
+        <Route path="/products/:category" element={<ProductList/>}/>
+        <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/success" element={<Success/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        {/* 
+        <Route path="/login">{user ? redirect("/") : <Login />}</Route>
+        <Route path="/register">
+          {user ? redirect("/")  : <Register />}
+        </Route> */}
+        </Routes>
+    </Router>
+  );
+};
+
+export default App;
