d11dde39aece811b04df974ab8acd217e55ee393
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2023-01-23 19:53:49 +0530

    pics added

--- src/data.jsx
+++ src/data.jsx
@@ -26,20 +26,20 @@ export const categories = [
   {
     id: 1,
     img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "SHIRT STYLE!",
+    title: "WOMEN",
     cat:"women"
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "LOUNGEWEAR LOVE",
-    cat:"coat"
+    img: "https://cdn.shopify.com/s/files/1/0022/0620/3948/files/Vetements_en_cuir_1.jpg?v=1666031370",
+    title: "MEN",
+    cat:"men"
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    title: "LIGHT JACKETS",
-    cat:"jeans"
+    img: "https://images.pexels.com/photos/1620765/pexels-photo-1620765.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "KIDS",
+    cat:"kids"
   },
 ];
 
