5499739eb4e73cbf8ccf3cf3bc7954ded947e4fb
Sharukh Dastageer Shaikh <shaikhsharukh1963@gmail.com>
2023-01-23 18:38:41 +0530

    update url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://rich-red-duckling-wrap.cyclic.app/";
+const BASE_URL = "https://rich-red-duckling-wrap.cyclic.app/api";
 const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYzhmZDU3Y2U0NzM3MGZkZmIyODI4NSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDIzOTk3OCwiZXhwIjoxNjc0NDk5MTc4fQ.b-VQwbXc3ElZNVtLnz34EjXXNYlQlY6Bq_sK8-qEcKs"
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
